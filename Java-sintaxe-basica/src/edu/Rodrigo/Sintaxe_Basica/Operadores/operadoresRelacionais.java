2be99d310517237e38d922d48aac97425a707168
RodrigoDosSantos <rodrigosantospx@gmail.com>
2023-08-30 19:09:25 -0300

    adição scanner no operador

--- Java-sintaxe-basica/src/edu/Rodrigo/Sintaxe_Basica/Operadores/operadoresRelacionais.java
+++ Java-sintaxe-basica/src/edu/Rodrigo/Sintaxe_Basica/Operadores/operadoresRelacionais.java
@@ -1,9 +1,18 @@
 package edu.Rodrigo.Sintaxe_Basica.Operadores;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class operadoresRelacionais {
      public static void main(String[] args) throws Exception {
-     int numero1= 1; 
-     int numero2 = 2;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US); // necessario para receber valores do usuario
+
+
+     System.out.println("Digite o primeiro número");
+     int numero1= scanner.nextInt(); 
+     System.out.println("Digite o segundo número");
+     int numero2 = scanner.nextInt();
      
      System.out.println("Numero 1 = " + numero1 + "  " + " Numero 2 = " + numero2);
      
@@ -21,23 +30,25 @@ public class operadoresRelacionais {
      resultado = numero1 != numero2;
           System.out.println( numero1 + " é diferente de " + numero2 + " ? " + resultado);
         System.out.println("======================================");
-        System.out.println("atribua outros valores as variaveis da linha 5 e 6 para testar mais números");
         System.out.println("---------------------------------------------------------------------------------------------------");
  
         System.out.println("Operadores lógicos:");
-        int num1 = 5; // altere para testar com outros números
-        int num2 = 3; // altere para testar com outros números
 
-        System.out.println(num1 + " > 4 &&(AND) " + num2 + " < 2 " );
-        if(num1 > 4 && num2 < 2){
+        System.out.println("Digite o primeiro número");
+        int num1 = scanner.nextInt(); // altere para testar com outros números
+        System.out.println("Digite o Segundo número");
+        int num2 = scanner.nextInt(); // altere para testar com outros números
+
+        System.out.println(num1 + " Maior que(>) 4 ||(OR) " + num2 + " Menor que(<) 20 " );
+        if(num1 > 4 && num2 < 20){
         System.out.println("Verdadeira, pois ambas condições são verdadeira");
         } else {
             System.out.println("Falsa, pois uma ou as 2 condições são falsas");
         }
 
-        System.out.println(num1 + " > 4 ||(OR) " + num2 + " < 2 " );
+        System.out.println(num1 + " Maior que(>) 4 ||(OR) " + num2 + " Menor que(<) 20 " );
 
-        if(num1 > 4 || num2 < 2){
+        if(num1 > 4 || num2 < 20){
            System.out.println("Verdadeira, pois uma ou ambas condições são verdadeiras");
         }else{
            System.out.println("Falso, pois nenhuma condição é verdadeira");
