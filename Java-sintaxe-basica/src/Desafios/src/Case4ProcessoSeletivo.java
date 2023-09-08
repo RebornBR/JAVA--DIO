9af70960596590d2117cf80a5957b6767da2324b
RodrigoDosSantos <rodrigosantospx@gmail.com>
2023-09-07 22:07:36 -0300

    z

--- Java-sintaxe-basica/src/Desafios/src/Case4ProcessoSeletivo.java
+++ Java-sintaxe-basica/src/Desafios/src/Case4ProcessoSeletivo.java
@@ -7,11 +7,9 @@ public class Case4ProcessoSeletivo {
 
     for (String candidato : candidatosAprovados){
     tentativasAtender(candidato); //candidato, substitui o parametro ligandoParaCandidatoNome
+  }  
+ }
 
-    }
-
-        
-    }
 /**
  * 
  * @return retorna true se o número aleatorio entre 0 e 3 for 1
@@ -19,7 +17,6 @@ public class Case4ProcessoSeletivo {
 static boolean pessoaAtendeu() {
 return new Random().nextInt(4)==1;	
 }
-
 
 /**
  * Método que executa um bloco, enquanto a condição for false e as tentativas realizadas forem menores que 3.
@@ -31,24 +28,20 @@ boolean atender = false; // inicialmente é falso
 int tentativasRealizadas = 1;
 do{
 atender = pessoaAtendeu(); // atender recebe o metodo pessoaAtendeu(), que retornará true, quando o número aleatório entre 0 e 3 for 1
-
-if(atender == true)
+     if(atender == true){
     System.out.println("Conseguimos contato com o candidato " + ligandoParaCandidatoNome);
-else 
+}else{
 tentativasRealizadas++; // soma 1 sempre que atender for false
-
-
-}while(atender == false && tentativasRealizadas < 3);// enquanto atender for igual a false && tentativas realizadas menores que 3, executará o bloco acima
-
-if(atender == true)// assim que, atender, que recebe o booleano do método pessoaAtendeu() for true exibira mensagem abaixo
-    System.out.println("O candidato: " + ligandoParaCandidatoNome + " atendeu após " + tentativasRealizadas + " tentativas."); 
-else
-    System.out.println("Não conseguimos contato com o candidato " + ligandoParaCandidatoNome + " Número de tentativas realizadas: " + tentativasRealizadas);//exibida, se atingirmos 3 tentativas, ou seja, se o atender for false.
-
-
 }
 
+} while(atender == false && tentativasRealizadas < 3);// enquanto atender for igual a false && tentativas realizadas menores que 3, executará o bloco acima
 
+      if(atender == true){// assim que, atender, que recebe o booleano do método pessoaAtendeu() for true exibira mensagem abaixo
+    System.out.println("O candidato: " + ligandoParaCandidatoNome + " atendeu após " + tentativasRealizadas + " tentativas.");
+}else{
+    System.out.println("Não conseguimos contato com o candidato " + ligandoParaCandidatoNome + ", Número de tentativas realizadas: " + tentativasRealizadas);//exibida, se atingirmos 3 tentativas, ou seja, se o atender for false.
+}
 
+}
 
 }
