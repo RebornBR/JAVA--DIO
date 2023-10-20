7f110564999382c4a5f37d43b045b192036051c5
RodrigoDosSantos <rodrigosantospx@gmail.com>
2023-10-19 23:01:34 -0300

    JPA

--- SpringBoot/aula-spring-data-jpa/src/main/java/dio/aula/repository/UserRepository.java
+++ SpringBoot/aula-spring-data-jpa/src/main/java/dio/aula/repository/UserRepository.java
@@ -2,7 +2,7 @@ package dio.aula.repository;
 
 import dio.aula.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+// User é o tipo, Integer é o tipo do id da nossa tabela
 public interface UserRepository extends JpaRepository<User, Integer> { /* nossa interface extends JpaRepository
 e adquire todos os seus métodos, segure ctrl e de um clique para ver os métodos da JpaRepository*/
 }
